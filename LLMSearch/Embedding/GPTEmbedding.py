4330172e5e94aa9961da3c5af7bb19172c52a8d2
KanHatakeyama <satokan@toki.waseda.jp>
2023-05-17 10:01:21 +0900

    gpt cool time

--- LLMSearch/Embedding/GPTEmbedding.py
+++ LLMSearch/Embedding/GPTEmbedding.py
@@ -1,12 +1,20 @@
 import openai
 import numpy as np
+import time
+
 
 class GPTEmbedding:
-    def __init__(self,api_key,model="text-embedding-ada-002") -> None:
-        self.model=model
-        openai.api_key =  api_key
-    def __call__(self,text):
-        v=openai.Embedding.create(input = [text], model=self.model)['data'][0]['embedding']
+    def __init__(self, api_key, model="text-embedding-ada-002",
+                 sleep_time=0.7) -> None:
+        self.model = model
+        openai.api_key = api_key
+        self.sleep_time = sleep_time
 
-        return np.array(v).reshape(1,-1).astype('float32')
+    def __call__(self, text):
+        v = openai.Embedding.create(input=[text], model=self.model)[
+            'data'][0]['embedding']
 
+        # this may be necessary to avoid the error of "Too Many Requests"
+        time.sleep(self.sleep_time)
+
+        return np.array(v).reshape(1, -1).astype('float32')
