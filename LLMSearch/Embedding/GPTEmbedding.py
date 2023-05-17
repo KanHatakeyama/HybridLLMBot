e0b39cf0ba7e1814fbc128a858da77fb72a1aa6b
KanHatakeyama <satokan@toki.waseda.jp>
2023-05-17 11:54:16 +0900

    max token

--- LLMSearch/Embedding/GPTEmbedding.py
+++ LLMSearch/Embedding/GPTEmbedding.py
@@ -5,7 +5,7 @@ import time
 
 class GPTEmbedding:
     def __init__(self, api_key, model="text-embedding-ada-002",
-                 sleep_time=0.7) -> None:
+                 sleep_time=0.9) -> None:
         self.model = model
         openai.api_key = api_key
         self.sleep_time = sleep_time
--- LLMSearch/GPTQuery.py
+++ LLMSearch/GPTQuery.py
@@ -2,19 +2,18 @@ import openai
 
 
 class GPTQuery:
-    def __init__(self,API_KEY,model="gpt-3.5-turbo") -> None:
+    def __init__(self, API_KEY, model="gpt-3.5-turbo") -> None:
         openai.api_key = API_KEY
-        self.model=model
+        self.model = model
 
-
-    def ask_gpt(self,query,context_text=None):
+    def ask_gpt(self, query, context_text=None):
         if context_text is not None:
-            messages=[
+            messages = [
                 {"role": "assistant", "content": context_text},
                 {"role": "user", "content": query},
             ]
         else:
-            messages=[
+            messages = [
                 {"role": "user", "content": query},
             ]
 
@@ -24,17 +23,15 @@ class GPTQuery:
         )
         return (response.choices[0]["message"]["content"].strip())
 
-    def reference_ask(self,query,context_list,k=2):
-        context_text=""
+    def reference_ask(self, query, context_list, k=2, ref_max_length=3500):
+        context_text = ""
         for i in range(k):
-            context_text+=context_list[i]["text"]+"."
+            context_text += context_list[i]["text"]+"."
 
+        context_text = context_text[:ref_max_length]
 
-        res_dict={}
-        res_dict["answer"]=self.ask_gpt(query,context_text)
-        res_dict["context"]=context_list[:k]
-
+        res_dict = {}
+        res_dict["answer"] = self.ask_gpt(query, context_text)
+        res_dict["context"] = context_list[:k]
 
         return res_dict
-
-
