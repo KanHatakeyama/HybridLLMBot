e69fbe0c2f8a3e4049666cd60179277a9c003860
KanHatakeyama <satokan@toki.waseda.jp>
2023-06-06 18:59:29 +0900

    ce

---
+++ LLMSearch/CrossEncoder.py
@@ -0,0 +1,31 @@
+
+from sentence_transformers.cross_encoder import CrossEncoder
+import torch
+import json
+
+
+# TODO: somehow, cross encoder init in the model does not work
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-12-v2', device=device)
+
+
+class CrossEncoder:
+    def __init__(self,
+                 setting_path='settings/settings.json',
+                 device="cpu"):
+
+        with open(setting_path) as f:
+            settings = json.load(f)
+        model_name = settings["CROSS_ENCODER_MODEL"]
+        #self.model = CrossEncoder(model_name, device=device)
+
+        #self.model = model
+
+    def predict(self, question, references):
+        pairs = [(question, reference[0][2]) for reference in references]
+
+        scores = model.predict(pairs)
+
+        # 類似度順にソートして返す
+        scores, pairs = zip(*sorted(zip(scores, pairs), reverse=True))
+        return scores, pairs
---
+++ LLMSearch/SQLTextDB.py
@@ -0,0 +1,79 @@
+from sqlite_utils import Database
+from . DocSplitter import clean_text, split_text
+import glob
+from tqdm import tqdm
+import json
+from transformers import BertJapaneseTokenizer
+# path_listでcountを取る
+from collections import Counter
+import nltk
+nltk.download('punkt')
+
+
+class SQLTextDB:
+    def __init__(self,
+                 setting_path='settings/settings.json',
+                 initiate=False) -> None:
+
+        with open(setting_path) as f:
+            settings = json.load(f)
+
+        self.db_path = settings["data_path"]+"/meta/text.db"
+        self.chunk_size_limit = settings["chunk_size_limit"]
+
+        self.tokenizer = BertJapaneseTokenizer.from_pretrained(
+            settings["JAPANESE_TOKENIZER_MODEL"])
+
+        self.db = Database(self.db_path, recreate=initiate)
+        if initiate:
+            self.add_record("path", "text", 0)
+            self.db["text"].enable_fts(["text"], tokenize="porter")
+            self.db["finished"].enable_fts(["path"], tokenize="porter")
+
+    def add_record(self, path, text, split_id):
+        self.db['text'].insert({
+            "path": path,
+            'split_id': split_id,
+            'text': text
+        })
+        if split_id == 0:
+            self.db['finished'].insert({"path": path})
+
+    def add_text(self, path):
+        finished_list = list(self.db["finished"].rows)
+        finished_list = [i["path"] for i in finished_list]
+        if path in finished_list:
+            print(f"{path} already added")
+            return
+
+        with open(path, 'r') as f:
+            text = f.read()
+
+        text = clean_text(text)
+
+        chunk_list = split_text(text, self.chunk_size_limit)
+        for i, chunk in enumerate(chunk_list):
+            self.add_record(path, chunk, i)
+
+    def search_text(self, query, k=10):
+
+        # tokenize japanese
+        wakati_ids = self.tokenizer.encode(query, return_tensors='pt')
+        jap_tokenized_list = self.tokenizer.convert_ids_to_tokens(
+            wakati_ids[0].tolist())
+        jap_tokenized_list = jap_tokenized_list[1:-1]
+
+        eng_tokenized_list = nltk.word_tokenize(query)
+        # search words
+        path_list = []
+        for tokenized_list in [jap_tokenized_list, eng_tokenized_list]:
+            for key in tokenized_list:
+                rows = self.db.execute(
+                    "SELECT * FROM text WHERE text LIKE ?", (f'%{key}%',))
+
+                temp_path_list = [row[:] for row in rows]
+                path_list.extend(temp_path_list)
+
+        c = Counter(path_list)
+        common_list = c.most_common(k)
+        return common_list
